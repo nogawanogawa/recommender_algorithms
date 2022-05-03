4ea0ddc16401e43da42616441faab0ed1fe04c15
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2022-05-03 20:08:53 +0900

    Add CF

---
+++ src/collaborative_filtering.py
@@ -0,0 +1,97 @@
+import dataloader
+import pandas as pd
+import numpy as np
+
+class CF:
+    def __init__ (self, rating:pd.DataFrame):
+        self.df = rating
+    
+    def user_similarity(self, df: pd.DataFrame, threshold=0.2) -> pd.DataFrame:
+        """ユーザーに関する相関係数
+        
+        df : 列がitem, 行がuserを示すDataFrame
+        threshold: 協調フィルタリングの計算に使用する際の相関係数の下限値
+        """
+        corr = df.T.corr() #ピアソンの相関係数
+        corr.values[[np.arange(corr.shape[0])]*2] = np.nan #対角線成分をnanに設定
+        sim = corr.where(corr > threshold, np.nan)
+        return sim 
+
+    def average_user_rating(self, df:pd.DataFrame) -> pd.DataFrame:
+        """avg_ratingをアイテムの個数分だけ拡張する
+
+        Args:
+            df (pd.DataFrame): ユーザーが付与した実際のレーティング
+            avg_rating (pd.Series): ユーザーごとの平均レーティング
+
+        Returns:
+            pd.DataFrame: avg_ratingをアイテムの個数分だけ拡張したDataFrame
+        """
+
+        avg_rating = df.T.mean()
+        S = pd.DataFrame(avg_rating)
+
+        for i in range(df.shape[1] -1):
+            S = pd.concat([S, pd.DataFrame(avg_rating)], axis=1)
+
+        # カラムのindexを1~アイテム数に振り直す
+        S = S.T.reset_index(drop = True)
+        S.index = np.arange(1, len(S)+1)
+        S = S.T
+        return S
+
+    def user_rating_diff(self, df:pd.DataFrame) -> pd.DataFrame:
+        """ユーザーごとの平均に対するレーティング
+
+        Args:
+            df (pd.DataFrame): ユーザーが付与した実際のレーティング
+            avg_rating (pd.Series): ユーザーごとの平均レーティング
+
+        Returns:
+            pd.DataFrame: ユーザーごとの平均に対するレーティング
+        """
+
+        # ユーザーごとのレーティングの平均値
+        avg_rating = df.T.mean()
+        y_ybar = df.copy()
+
+        for i in range(df.shape[1]):
+            y_ybar.loc[:, i+1] = y_ybar.loc[:, i+1] - avg_rating
+
+        return y_ybar
+    
+    def weight_sum(self, sim:pd.DataFrame, df: pd.DataFrame):
+        
+        # ユーザーが付与した実際のレーティングを、評価していれば1, 評価していなければ0に変換 
+        df_ = df.where(df.isna(), 1)
+        df_ = df_.fillna(0)
+
+        # ユーザーの類似度と内積を取ることで、自分を除く類似したユーザーの類似度の合計を求める
+        d = sim.fillna(0).dot(df_.fillna(0))
+        return d
+
+    def run(self):
+        """協調フィルタリングの計算
+
+        Returns:
+            _type_: _description_
+        """
+
+        df = self.df
+        
+        sim = self.user_similarity(df)
+        avg_user_rating = self.average_user_rating(df)
+        y_ybar = self.user_rating_diff(df)
+
+        d = self.weight_sum(sim, df)
+        c = sim.fillna(0).dot(y_ybar.fillna(0))
+        
+        return avg_user_rating.add((c/d))
+
+if __name__ == '__main__':
+    df = dataloader.data()
+    df = df.pivot_table(index='use_id', columns='item_id', values='rating')
+    
+    cf = CF(df)
+    result = cf.run()
+    print(result)
