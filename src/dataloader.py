6a25213230b925ee8542d62f8e40890a3bf831f8
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2022-05-05 21:55:30 +0900

    Add lgbm

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,12 +2,12 @@ import pandas as pd
 import os
 
 def user(homedir='/home') -> pd.DataFrame:
-    cols = ['use_id', 'age', 'gender', 'occupation', 'zip_code']
+    cols = ['user_id', 'age', 'gender', 'occupation', 'zip_code']
     df = pd.read_csv(os.path.join(homedir, 'ml-100k/u.user'), sep='|', header=None, names=cols)
     return df
 
 def data(homedir='/home') -> pd.DataFrame:
-    cols = ['use_id', 'item_id', 'rating', 'timestamp']
+    cols = ['user_id', 'item_id', 'rating', 'timestamp']
     df = pd.read_csv(os.path.join(homedir, 'ml-100k/u.data'), sep='\t', header=None, names=cols)
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
     return df
@@ -20,7 +20,7 @@ def genre(homedir='/home') -> pd.Series:
 
 def item(homedir='/home') -> pd.DataFrame:
     genre_names = genre(homedir).values.tolist()
-    cols = ['movie_id', 'movie_title', 'release_date', 'video_release_date', 'IMDb URL']
+    cols = ['item_id', 'movie_title', 'release_date', 'video_release_date', 'IMDb URL']
     cols.extend(genre_names)
 
     return pd.read_csv(os.path.join(homedir, 'ml-100k/u.item'), sep='|', header=None, names=cols, encoding="ISO-8859-1")
